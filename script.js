41ce35e575edc6a9b99660d698a4b11f6bff94b3
Brian Toney <btoney1988@gmail.com>
2020-09-24 19:28:49 -0400

    Small adjustment to make it work correctly

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// Global Variables
+var city = "";
 var stateInput = "ca";
 var eventInput = "baseball";
 var startEndDateTime = "2020-09-27" + "T00:00:00Z";
 
+// Ticketmaster Ajax call
 $.ajax({
   url: "https://app.ticketmaster.com/discovery/v2/events.json",
   data: {
@@ -13,7 +16,6 @@ $.ajax({
   method: "GET"
 }).then(function (response) {
   console.log(response);
-
   for ( var x = 0; x < response._embedded.events.length; x++){
     // Local Event Variables
     var event = response._embedded.events[x];
@@ -54,21 +56,5 @@ $.ajax({
 
     $("#event-hotel-info").append(eventCard);
   }
-});
 
-var query = "new york"
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + query,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "hotels4.p.rapidapi.com",
-		"x-rapidapi-key": "111d87e276msh7a7e68922114d72p183e89jsnda0c04325211"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-  console.log(response.suggestions[3].entities[0].name)
 });
