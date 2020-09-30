0c8ac417b953b9c6f921593b36cf9ffebf559a64
Ryan Wilson <ryanswilson1@gmail.com>
2020-09-29 22:56:15 -0400

    changes to hotel function

--- script.js
+++ script.js
@@ -75,15 +75,9 @@ function getTicketData() {
       "apikey": "xJY9ixix03PyEzTVRHSf0eldysSBFkoN",
       "radius": "500",
       "keyword": "sports",
-<<<<<<< HEAD
-      "city": 'san jose',
-      "stateCode": 'ca',
-      "classificationName": 'soccer',
-=======
       "city": city,
       "stateCode": stateInput,
       "classificationName": eventInput,
->>>>>>> master
       "localStartDateTime": "2020-09-30T00:00:00",
     },
     method: "GET"
